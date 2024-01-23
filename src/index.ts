bb26f23bce6b7734d62ae943c6992e0dcebabf61
Renass21 <renatodossantoss21@gmail.com>
2024-01-23 18:04:17 -0300

    login-controller

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express  from "express";
 import cors from "cors";
-
+import { LoginController } from "./controllers/login.controller";
+import { UserController } from "./controllers/user.controller";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+const loginController = new LoginController();
+const userController = new UserController();
 
-
+app.post("/signup", userController.user)
+app.post("/login", loginController.login)
 
 
 app.listen(3000, () => {

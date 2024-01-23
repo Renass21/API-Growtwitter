45ecc7db4b84c2287288c9eec8e19a9155100ed1
Renass21 <renatodossantoss21@gmail.com>
2024-01-23 18:00:52 -0300

    controller_user

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import { errorBadRequest, serverError } from "../util/response.helper";
+import { error } from "console";
+import { User } from "../models/user.model";
+import repository from "../database/prisma.repository";
+
+export class UserController{
+    public async user(req: Request, res: Response){
+       try {
+        const {name, username, email, password} = req.body;
+        if(!name || !username || !email || !password){
+            return errorBadRequest(res)
+        }
+        const user = new User(name, username, email, password);
+
+        const result = await repository.user.create({
+            data: user
+        });
+
+        return res.status(201).send({
+            ok: true,
+            message: "User created successfully",
+            data: result
+        })
+
+
+       } catch (error) {
+        return serverError(res,error);
+       }
+    }
+
+}

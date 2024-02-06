17f9797e45942d2afda735d2ad3690bf009291f6
Renass21 <renatodossantoss21@gmail.com>
2024-02-06 00:06:40 -0300

    middlewares

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,10 +1,34 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { serverError } from "../util/response.helper";
+import repository from "../database/prisma.repository";
 
-export async function authLoginMiddleware(req: Request, res: Response) {
+export async function authLoginMiddleware(req: Request, res: Response, next: NextFunction) {
     try {
-        
-    } catch (error: any) {
+        const { authorization } = req.headers;
+        const { idUser } = req.params;
+
+        if (!authorization) {
+            return res.status(401).send({ 
+                ok: false,
+                message: "Token doesn't exist." 
+            });
+        }
+         
+        const user = await repository.user.findUnique({
+            where: {
+                idUser,
+            },
+        });
+
+        if (!user || user.token !== authorization) {
+            return res.status(401).send({ 
+                ok: false,
+                message: "Token invalid or unauthorized." 
+            });
+        }
+            next();
+
+        } catch (error: any) {
         serverError(res,error);
     }
 }
